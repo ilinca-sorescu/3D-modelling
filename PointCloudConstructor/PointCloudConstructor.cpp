234f9dd54eb22f6de723b05b9ebcbb522c033bf2
ilinca_arch <ilinca.sorescu@gmail.com>
2015-02-24 02:29:22 +0000

    restructured computePoints to allow for multiple strategies

--- PointCloudConstructor/PointCloudConstructor.cpp
+++ PointCloudConstructor/PointCloudConstructor.cpp
@@ -14,6 +14,7 @@
 #include <pcl/io/pcd_io.h>
 
 #define white 16777215
+#define epsilon 0.001
 
 using namespace std;
 using namespace cv;
@@ -62,15 +63,15 @@ PointCloudConstructor::PointCloudConstructor(
 
 //  normalizeCloudValues();
 
-  populateCloud(this->getPoints());
-  cout<<cloud->points.size()<<" points before radius outlier removal."<<endl;
+  populateCloud(this->computePoints());
+ /* cout<<cloud->points.size()<<" points before radius outlier removal."<<endl;
   cloud = RadiusOutlierRemovalFilter(cloud).getFilteredCloud();
   cout<<cloud->points.size()<<" points after radius outlier removal."<<endl;
 
   cout<<cloud->points.size()<<" points before statistical removal."<<endl;
   cloud = StatisticalRemovalFilter(cloud).getFilteredCloud();
   cout<<cloud->points.size()<<" points after statistical removal."<<endl;
-
+*/
 }
 
 void PointCloudConstructor::compute_kclosest() {
@@ -90,7 +91,7 @@ void PointCloudConstructor::compute_kclosest() {
   }
 }
 
-double inline abs(double x) {
+double inline absv(double x) {
   return x<0?-x:x;
 }
 
@@ -102,9 +103,9 @@ void PointCloudConstructor::normalizeCloudValues() {
   double maxv = 0;
   for(auto p:cloud->points) {
       maxv = max(maxv,
-        max(abs(p.x),
-          max(abs(p.y),
-            abs(p.z))));
+        max(absv(p.x),
+          max(absv(p.y),
+            absv(p.z))));
   }
   cout<<maxv<<endl;
   for(auto p:cloud->points) {
@@ -132,11 +133,14 @@ Point3d PointCloudConstructor::triangulate(
             -p2.x*C2(2, 3)+C2(0, 3),
             -p2.y*C2(2, 3)+C2(1, 3));
 
-  solve(A, B, X, DECOMP_SVD);
+  //solve(A, B, X, DECOMP_SVD);
 
-  if(X(0) > 40 || X(0) < -40 ||
-     X(1) > 40 || X(1) < -40 ||
-     X(2) > 40 || X(2) < -40) {
+  X = (A.t()*A).inv()*A.t()*B;
+
+
+ /* if(X(0) > 10 || X(0) < -10 ||
+     X(1) > 10 || X(1) < -10 ||
+     X(2) > 10 || X(2) < -10) {
     cout<<"point3d: "<<X(0)<<" "<<X(1)<<" "<<X(2)<<endl;
     cout<<"point2d: "<<p1.x<<" "<<p1.y<<" "<<i<<" "<<
       reprojectionError(C1, p1, Point3d(X(0), X(1), X(2)))<<endl;
@@ -146,7 +150,83 @@ Point3d PointCloudConstructor::triangulate(
     cout<<R(0)<<" "<<R(1)<<" "<<R(2)<<" "<<R(3)<<endl;
     cout<<B(0)<<" "<<B(1)<<" "<<B(2)<<" "<<B(3)<<endl<<endl;
   }
+*/
+  return Point3d(X(0), X(1), X(2));
+}
 
+double distance(Point3d a, Point3d b) {
+  return sqrt((a.x-b.x)*(a.x-b.x) +
+              (a.y-b.y)*(a.y-b.y) +
+              (a.z-b.z)*(a.z-b.z));
+}
+
+Point3d PointCloudConstructor::ReadjustedTriangulation(
+    Matx34d C1, Matx34d C2, Point2d p1, Point2d p2, int i, int j) {
+
+
+  Point3d initialX = triangulate(C1, C2, p1, p2, i, j);
+  Matx31d X(initialX.x, initialX.y, initialX.z);
+  int noOfIterations = 0;
+  double w1 = 1.0, w2 = 1.0;
+  while(noOfIterations < 10) {
+    noOfIterations++;
+
+    double newW1 = C1(2, 0)*X(0) + C1(2, 1)*X(1) + C1(2, 2)*X(2) + C1(2, 3)*X(3);
+    double newW2 = C2(2, 0)*X(0) + C2(2, 1)*X(1) + C2(2, 2)*X(2) + C2(2, 3)*X(3);
+
+    if(absv(w1-newW1) < epsilon && absv(w2-newW2) < epsilon)
+      break;
+
+    w1 = newW1;
+    w2 = newW2;
+
+    //readjust the weights of the two equations
+    Matx43d A((p1.x*C1(2, 0)-C1(0, 0))/w1, (p1.x*C1(2, 1)-C1(0, 1))/w1, (p1.x*C1(2, 2)-C1(0, 2))/w1,
+              (p1.y*C1(2, 0)-C1(1, 0))/w1, (p1.y*C1(2, 1)-C1(1, 1))/w1, (p1.y*C1(2, 2)-C1(1, 2))/w1,
+              (p2.x*C2(2, 0)-C2(0, 0))/w2, (p2.x*C2(2, 1)-C2(0, 1))/w2, (p2.x*C2(2, 2)-C2(0, 2))/w2,
+              (p2.y*C2(2, 0)-C2(1, 0))/w2, (p2.y*C2(2, 1)-C2(1, 1))/w2, (p2.y*C2(2, 2)-C2(1, 2))/w2);
+
+    Matx41d B((-p1.x*C1(2, 3)+C1(0, 3))/w1,
+              (-p1.y*C1(2, 3)+C1(1, 3))/w1,
+              (-p2.x*C2(2, 3)+C2(0, 3))/w2,
+              (-p2.y*C2(2, 3)+C2(1, 3))/w2);
+
+//    Matx31d aux;
+//    solve(A, B, aux, DECOMP_SVD);
+    X = (A.t()*A).inv()*A.t()*B;
+
+//    double error = max(reprojectionError(C1, p1, Point3d(X(0), X(1), X(2))),
+//                     reprojectionError(C2, p2, Point3d(X(0), X(1), X(2))));
+
+/*    if((initialX.x > 12 || initialX.x < -12 ||
+        initialX.y > 12 || initialX.y < -12 ||
+        initialX.z > 12 || initialX.z < -12)) {
+      Matx41d R = A*X;
+      cout<<"initialX: "<<initialX.x<<" "<<initialX.y<<" "<<initialX.z<<endl;
+      cout<<"point3d: "<<X(0)<<" "<<X(1)<<" "<<X(2)<<endl;
+      cout<<"point2d: "<<p1.x<<" "<<p1.y<<" "<<i<<" "<<
+        reprojectionError(C1, p1, Point3d(X(0), X(1), X(2)))<<endl;
+      cout<<"point2d: "<<p2.x<<" "<<p2.y<<" "<<j<<" "<<
+        reprojectionError(C2, p2, Point3d(X(0), X(1), X(2)))<<endl;
+      cout<<"distance: "<<distance(images[i]->getCameraPose(),
+                                   images[j]->getCameraPose())<<endl;
+      cout<<"AX: "<<R(0)<<" "<<R(1)<<" "<<R(2)<<" "<<R(3)<<endl;
+      cout<<"B: "<<B(0)<<" "<<B(1)<<" "<<B(2)<<" "<<B(3)<<endl;
+      cout<<"A: "<<endl;
+      cout<<A(0, 0)<<" "<<A(0, 1)<<" "<<A(0, 2)<<endl;
+      cout<<A(1, 0)<<" "<<A(1, 1)<<" "<<A(1, 2)<<endl;
+      cout<<A(2, 0)<<" "<<A(2, 1)<<" "<<A(2, 2)<<endl;
+      cout<<A(3, 0)<<" "<<A(3, 1)<<" "<<A(3, 2)<<endl<<endl;
+    }*/
+  //  cout<<"reweighted X: "<<X(0)<<" "<<X(1)<<" "<<X(2)<<" "<<w1<<" "<<w2<<endl;
+  }
+
+  /*if(noOfIterations > 1) {
+    cout<<"hola: "<<noOfIterations<<endl;
+    cout<<tX.x<<" "<<tX.y<<" "<<tX.z<<endl;
+    cout<<X(0)<<" "<<X(1)<<" "<<X(2)<<endl<<endl;
+  }
+  */
   return Point3d(X(0), X(1), X(2));
 }
 
@@ -157,7 +237,7 @@ vector<Point3d> PointCloudConstructor::filterByReprojectionError(
   vector<Point3d> good_points;
   for(auto i = 0u; i != points.size(); ++i)
     //if the reprojection error is small
-    if(points[i].second <= 0.0001)
+    if(points[i].second <= 0.0003)
       good_points.push_back(points[i].first);
 
   cout<<"Before filtering by reprojection error: "<<points.size()<<endl;
@@ -184,12 +264,67 @@ double PointCloudConstructor::reprojectionError(
   return distance(x, xReprojected);
 }
 
-vector<Point3d> PointCloudConstructor::getPoints() {
+//decides which strategy is used for computing points
+vector<Point3d> PointCloudConstructor::computePoints() {
+  return computePoints_kclosest();
+}
+
+vector<PointCloudConstructor::TriangulatedPoint> PointCloudConstructor::computePointsBetweenTwoImgs(int i, int j) {
+      vector<DMatch> matches = featureMatcher->match(i, j);
+
+      Matx34d cameraM1 = images[i]->getCameraMatrix();
+      Matx34d cameraM2 = images[j]->getCameraMatrix();
+
+      vector<TriangulatedPoint> tPoints;
+
+      for(const auto &m: matches) {
+
+        //Get the 2D locations(in image coordinates) corresponding to the current match
+        Point2d p1, p2;
+        FeatureMatcher::cameraCoordinatesOfDMatch(m, images[i], images[j], p1, p2);
+
+        TriangulatedPoint tp;
+        tp.point = ReadjustedTriangulation(cameraM1, cameraM2, p1, p2, i, j);
+        tp.reprojectionError = max(reprojectionError(cameraM1, p1, tp.point),
+                                   reprojectionError(cameraM2, p2, tp.point));
+        tp.imgId1 = i;
+        tp.imgId2 = j;
+        tp.featureId1 = m.queryIdx;
+        tp.featureId2 = m.trainIdx;
+        tPoints.push_back(tp);
+      }
+
+      return tPoints;
+}
+
+vector<Point3d> PointCloudConstructor::computePoints_kclosest() {
   cout<<"***Generating the point cloud***"<<endl;
 
   vector<pair<Point3d, double>> points3D;
 
   compute_kclosest();
+
+ /*double s=0, min=15, maxi=0, d;
+  vector<pair<double, int>> distances;
+  for(auto i=0u; i != images.size(); ++i) {
+    d = distance(images[i]->getCameraPose(),
+                 images[kclosest[i][0]]->getCameraPose());
+    s += d;
+    if(min > d)
+      min = d;
+    if(maxi < d)
+      maxi = d;
+    distances.push_back(make_pair(d, i));
+  }
+  sort(distances.begin(), distances.end());
+  for(auto dist:distances) {
+    cout<<dist.first<<" "<<dist.second<<endl;
+  }
+  cout<<s/images.size()<<" "<<maxi<<" "<<min<<" "<<
+    distance(images[306]->getCameraPose(),
+             images[984]->getCameraPose())<<endl;
+*/
+
   for(auto i=0u; i != images.size(); ++i) {
 
     //generatedPoints[x] = the 3D points resulted from triangulating
@@ -205,45 +340,33 @@ vector<Point3d> PointCloudConstructor::getPoints() {
 
       //index of jth closest image to i
       int jthImage = kclosest[i][j];
-      matches[i][j] = featureMatcher->match(i, jthImage);//, true); //delete true!
-      vector<DMatch> &currentMatches = matches[i][j];
+      vector<TriangulatedPoint> tPoints = computePointsBetweenTwoImgs(i, jthImage);
+      for(auto tp:tPoints)
+        generatedPoints[tp.featureId1].push_back(make_pair(
+              tp.point,
+              tp.reprojectionError));
+    }
 
-      for(const auto &m: currentMatches) {
-        Matx34d cameraM1 = images[i]->getCameraMatrix();
-        Matx34d cameraM2 = images[jthImage]->getCameraMatrix();
-
-        //Get the 2D locations(in image coordinates) corresponding to the current match
-        Point2d p1, p2;
-        FeatureMatcher::cameraCoordinatesOfDMatch(m, images[i], images[jthImage], p1, p2);
-
-        Point3d X = triangulate(cameraM1, cameraM2, p1, p2, i, jthImage);
-        generatedPoints[m.queryIdx].push_back(make_pair(
-            X,
-            max(reprojectionError(cameraM1, p1, X),
-                reprojectionError(cameraM2, p2, X))));
+    //compute the average of the generatedPoints for each trainIdx
+    for(auto gp:generatedPoints) {
+      Point3d sum3D(0, 0, 0);
+      double errorSum = 0;
+      for(auto p:gp) {
+        sum3D += p.first;
+        errorSum += p.second;
       }
-
-      //compute the average of the generatedPoints for each trainIdx
-      for(auto gp:generatedPoints) {
-        Point3d sum3D(0, 0, 0);
-        double errorSum = 0;
-        for(auto p:gp) {
-          sum3D += p.first;
-          errorSum += p.second;
-        }
-        if(gp.size() != 0) {
-          Point3d X(sum3D.x/gp.size(),
-                    sum3D.y/gp.size(),
-                    sum3D.z/gp.size());
-          double reprojectionError = errorSum/gp.size();
-          points3D.push_back(make_pair(X, reprojectionError));
-        }
+      if(gp.size() != 0) {
+        Point3d X(sum3D.x/gp.size(),
+                  sum3D.y/gp.size(),
+                  sum3D.z/gp.size());
+        double reprojectionError = errorSum/gp.size();
+        points3D.push_back(make_pair(X, reprojectionError));
       }
     }
   }
 
   //DEBUG
-  featureMatcher->match(0, kclosest[0][0], true);
+  //featureMatcher->match(984, kclosest[984][0], true);
   cout<<"The point cloud was successfully generated!"<<endl;
   return filterByReprojectionError(points3D);
 }
@@ -307,6 +430,26 @@ int main(int argc, char *argv[]) {
     pcc = new PointCloudConstructor(folder, NumberOfPics);
   pcc->cloudToTxt("cloud.txt");
   pcc->cloudToPCD("cloud.pcd");
+
+ /* Matx34d C1 = pcc->getImages()[306]->getCameraMatrix();
+  Point3d p1 = pcc->getImages()[306]->getCameraPose();
+  Matx31d R1 = C1*Matx41d(p1.x, p1.y, p1.z, 1);
+  cout<<"p1: "<<p1.x<<" "<<p1.y<<" "<<p1.z<<endl;
+  cout<<"C1: "<<C1(0, 0)<<" "<<C1(0, 1)<<" "<<C1(0, 2)<<" "<<C1(0, 3)<<endl
+              <<C1(1, 0)<<" "<<C1(1, 1)<<" "<<C1(1, 2)<<" "<<C1(1, 3)<<endl
+              <<C1(2, 0)<<" "<<C1(2, 1)<<" "<<C1(2, 2)<<" "<<C1(2, 3)<<endl;
+  cout<<"R1: "<<R1(0, 0)<<" "<<R1(1, 0)<<" "<<R1(2, 0)<<endl<<endl;
+
+
+  Matx34d C2 = pcc->getImages()[984]->getCameraMatrix();
+  Point3d p2 = pcc->getImages()[984]->getCameraPose();
+  Matx31d R2 = C2*Matx41d(p2.x, p2.y, p2.z, 1);
+  cout<<"p2: "<<p2.x<<" "<<p2.y<<" "<<p2.z<<endl;
+  cout<<"C2: "<<C2(0, 0)<<" "<<C2(0, 1)<<" "<<C2(0, 2)<<" "<<C2(0, 3)<<endl
+              <<C2(1, 0)<<" "<<C2(1, 1)<<" "<<C2(1, 2)<<" "<<C2(1, 3)<<endl
+              <<C2(2, 0)<<" "<<C2(2, 1)<<" "<<C2(2, 2)<<" "<<C2(2, 3)<<endl;
+  cout<<"R2: "<<R2(0, 0)<<" "<<R2(1, 0)<<" "<<R2(2, 0)<<endl<<endl;
+*/
   delete pcc;
 //  PointCloudConstructor *pcc;
 //  if(argc < 2)
@@ -328,10 +471,10 @@ int main(int argc, char *argv[]) {
   }
   cout<<minx<<" "<<miny<<" "<<maxx<<" "<<maxy<<endl;
   */
-  //vector<Point3d> points = pcc->getPoints();
+  //vector<Point3d> points = pcc->computePoints();
 //  exportPCLCloud(
 //      statisticalRemovalFilter(
-//        populatePCLCloud(pcc->getPoints())),
+//        populatePCLCloud(pcc->computePoints())),
 //      "cloud.PCD");
 /*  ofstream out;
   out.open("cloud.txt");
--- PointCloudConstructor/PointCloudConstructor.h
+++ PointCloudConstructor/PointCloudConstructor.h
@@ -25,14 +25,35 @@ class PointCloudConstructor {
         int wantedNumberOfPics = -1);
    	std::vector<std::shared_ptr<Image>> getImages();
     Point3d triangulate(Matx34d, Matx34d, Point2d, Point2d, int, int);
+    Point3d ReadjustedTriangulation(Matx34d, Matx34d, Point2d, Point2d, int, int);
     void populateCloud(std::vector<cv::Point3d>);
     void cloudToPCD(String PCDFileName); //Point Cloud Data (out) file
     void cloudToTxt(String txtFileName);
     void normalizeCloudValues(); //make all point coords. in [-1, 1]
 
   private:
-    std::vector<cv::Point3d> getPoints();
-		std::vector<std::shared_ptr<Image>> images;
+    struct TriangulatedPoint{
+      Point3d point;
+      double reprojectionError;
+      int imgId1;
+      int imgId2;
+      int featureId1;
+      int featureId2;
+    };
+
+    //for each picture search for matches in its k closest neighbours
+    std::vector<cv::Point3d> computePoints_kclosest();
+
+    //search for matches in all of the pairs of images (img1, img2) where
+    //the ratio between the distance between cameras 1 and 2 and the radius
+    //of the viewing sphere is in the specified interval
+    std::vector<cv::Point3d> computePoints_ratioInterval();
+
+    std::vector<cv::Point3d> computePoints();
+
+    std::vector<TriangulatedPoint> computePointsBetweenTwoImgs(int, int);
+
+    std::vector<std::shared_ptr<Image>> images;
     std::unique_ptr<FeatureMatcher> featureMatcher;
     std::vector<std::vector<int>> kclosest;
     std::vector<std::vector<std::vector<cv::DMatch>>> matches;
